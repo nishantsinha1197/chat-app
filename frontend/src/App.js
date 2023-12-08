349b0e9aae83b3b45204c03ff17c3b383868dd6a
nishantsinha1197 <nishantsinha1197@gmail.com>
2023-12-08 23:54:57 +0530

    Adding commits for react chat app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChatContainer from './components/ChatContainer'
+
+function App() {
+  return (
+    <div style={{backgroundColor:'#ece5dd',height:'100vh', padding:'10px'}}>
+      <ChatContainer />
+    </div>
+  )
+}
+
+export default App
